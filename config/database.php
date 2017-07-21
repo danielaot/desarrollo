73b7b07918e7b3988f6488dc73e431e49f4283ab
Juliana Palacios <japalacios@bellezaexpress.com>
2017-07-21 10:14:35 -0500

    Se agrega conexion a bd_desarrollo

--- config/database.php
+++ config/database.php
@@ -106,6 +106,20 @@ return [
             'strict' => true,
             'engine' => 'InnoDB',
         ],
+
+        'desarrollo' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => 'bd_desarrollo',
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_spanish2_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => 'InnoDB',
+        ],
     ],
 
     /*
