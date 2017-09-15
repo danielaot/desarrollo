53fd58d7ee519780d9a76d5abdcccec4f49fd633
Juliana Palacios <japalacios@bellezaexpress.com>
2017-09-15 08:44:06 -0500

    cambio de imagen en el cabezote

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,5 +1,5 @@
 <!-- Ini - Encabezado -->
 <header class="txt-center">
-  <img src="{{url('/images/banner.png')}}" style="width:80%;"/>
+  <img src="{{url('/images/bellezaexpress.jpg')}}" style="width:30%;"/>
 </header>
 <!-- Fin - Encabezado -->
