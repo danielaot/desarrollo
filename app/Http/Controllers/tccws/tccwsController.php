8e0d75165d3cc47d23e78305670680738ec56b09
Juan David Marcillo <jdmarcillo@bellezaexpress.com>
2018-01-09 11:23:48 -0500

    subo nuevamente la fecha

--- app/Http/Controllers/tccws/tccwsController.php
+++ app/Http/Controllers/tccws/tccwsController.php
@@ -53,7 +53,7 @@ class tccwsController extends Controller
         'nom_tercero', 'num_sucursal', 'desc_sucursal',
         'nit_tercero', 'date_creacion','tipoPedido')
         ->whereNull('fecha_remesa')
-        ->where('date_creacion', '>', '08-01-2018')
+        ->where('date_creacion', '>', '05-01-2018')
         ->whereNotIn('tipo_docto', ['F30', 'F28', 'F31', 'F48'])
         ->distinct()->orderBy('num_factura')->get();
 
